8cbaeeb84c015c08b85b22666315f23ebbddb0d3
Wwinner <fernandowwinner.97@gmail.com>
2021-12-11 23:21:42 -0500

    Front-End 0.1

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,41 +3,11 @@ import { useState } from "react";
 
 
 export default function login(){
-
-    const [credencial,setCredencial]= useState({
-        email:'',
-        password:''
-    });
-
-    function handeSubmit(e){
-        e.preventDefault();
-
-        console.log(email)
-        
-    }
-    
     
     return(
         <>
-            <h2>Login</h2>
-
-            <form action='Post' onSubmit={handeSubmit}>
-                <input 
-                    type='email' 
-                    onChange={ e=> setCredencial(...email , e.target.value )}>
-                </input>
-                <input type='password' onChange={e=> setCredencial(...password ,e.target.value)}></input>
-                <button type='submit'>Connect</button>
-            </form>    
-        
+            <h2>Login</h2>  
         </>
     );
-}
-
-function getServerSide(req){
-
-    Param:{
-
-    }
 }
 
