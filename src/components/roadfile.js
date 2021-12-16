70fc21633aa77158b8d8b8f6fffc950819f9e360
Wwinner <fernandowwinner.97@gmail.com>
2021-12-16 06:32:48 -0500

    front-end 0.2.2 icon circle

--- src/components/roadfile.js
+++ src/components/roadfile.js
@@ -1,14 +1,16 @@
 import styles from '../styles/components/roadfile.module.css'
+import {FaFile,BiFile , FiVideo, FiImage, FiMusic , FaTasks} from 'react-icons/all'
+
 
 export default function Roadfile(){
     return(
         <div className={styles.road}>
             <div className={styles.circle}>
-                <div className={styles.icon0}></div>
-                <div className={styles.icon1}></div>
-                <div className={styles.icon2}></div>
-                <div className={styles.icon3}></div>
-                <div className={styles.icon4}></div>
+                <div className={styles.icon0}><a href=""><BiFile/></a></div>
+                <div className={styles.icon1}><a href=""><FiVideo/></a></div>
+                <div className={styles.icon2}><a href=""><FiImage/></a></div>
+                <div className={styles.icon3}><a href=""><FiMusic/></a></div>
+                <div className={styles.icon4}><a href=""><FaTasks/></a></div>
             </div>
         </div>
     );
